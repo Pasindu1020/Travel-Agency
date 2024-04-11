1a707ed775c6dba3b74d517395fcabe2d6514c7a
Pasindu1020 <151561180+Pasindu1020@users.noreply.github.com>
2024-04-11 20:51:16 +0530

    backend

---
+++ Login.php
@@ -0,0 +1,21 @@
+<?php
+     $firstname = $_POST['fname'];
+     $lastname = $_POST['lname'];
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+
+     //Database Connection
+
+     $conn = new mysqli('localhost', 'root', '', 'travelagency');
+     if($conn->connect_error){
+        die('Connection Failed :'.$conn->connect_error);
+     } else {
+        $stmt = $conn->prepare("insert into Users(Firstname, Lastname, Email, Password) values (?,?,?,?)");
+        $stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
+        $stmt->execute();
+        echo "Registration Succcessfully";
+        $stmt->close();
+        $conn->close();
+     }
+
+?>
