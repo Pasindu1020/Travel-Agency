7f90b558fe30f2fe06aa53a886254930dde7c5a9
Pasindu1020 <151561180+Pasindu1020@users.noreply.github.com>
2024-04-16 22:31:46 +0530

    modify

--- Booking.php
+++ Booking.php
@@ -11,15 +11,18 @@
      if($conn->connect_error){
         die('Connection Failed :'.$conn->connect_error);
      } else {
-        $getUserIdQuery = "SELECT id FROM users WHERE email = ?";
-        $stmt = $mysqli->prepare($getUserIdQuery);
+        $getUserIdQuery = "SELECT UserID FROM users WHERE email = ?";
+        $stmt = $conn->prepare($getUserIdQuery);
         $stmt->bind_param("s", $email);
         $stmt->execute();
         $stmt->bind_result($userId);
         $stmt->fetch();
+        $stmt->close();
+
         $stmt = $conn->prepare("insert into Booking(Contact_No, Checkin_date, Checkout_date, User_id) values (?,?,?,?)");
         $stmt->bind_param("issi", $mobilenum, $checkin, $checkout, $userId);
         $stmt->execute();
+        header("Location: Payment.html");
         $stmt->close();
         $conn->close();
      }
