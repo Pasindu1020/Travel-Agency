bf7c58c65d17a3b8ec6b51f48e8a5385e197753f
Shenuka0816 <shenukasolangaarachchi@gmail.com>
2024-04-17 22:06:44 +0530

    check

---
+++ Review.php
@@ -0,0 +1,22 @@
+<?php
+     $review_id = $_POST['reviewid'];
+     $lastname = $_POST['lname'];
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+
+
+     //Database Connection
+
+     $conn = mysqli_connect('localhost', 'root', '', 'travelagency');
+     if($conn->connect_error){
+        die('Connection Failed :'.$conn->connect_error);
+     } else {
+        $stmt = $conn->prepare("insert into Users(Firstname, Lastname, Email, Password) values (?,?,?,?)");
+        $stmt->bind_param("ssss", $firstname, $lastname, $email, $password);
+        $stmt->execute();
+        echo "<script>alert('Successfully registered'); window.location.href='Login.html';</script>";
+        $stmt->close();
+        $conn->close();
+     }
+
+?>
