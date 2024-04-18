e3a3c9405f3cc19fae5267fa4330343d2cc4ddc2
Pasindu1020 <151561180+Pasindu1020@users.noreply.github.com>
2024-04-18 11:20:55 +0530

    modify

--- Login.php
+++ Login.php
@@ -9,19 +9,25 @@
      if($conn->connect_error){
         die('Connection Failed :'.$conn->connect_error);
      } else {
-        $useremail  = mysqli_real_escape_string($conn, $useremail );
+        $useremail = mysqli_real_escape_string($conn, $useremail);
         $userpassword = mysqli_real_escape_string($conn, $userpassword);
-
-         $sql = "SELECT * FROM Users WHERE Email='$useremail' AND Password='$userpassword'";
-         $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $_SESSION['logemail'] = $useremail;
-        header("Location: HomeLogged.html");
-        exit;
-    } else {
-        echo "<script>alert('Invalid username or password'); window.location.href='Login.html';</script>";
-    }
+    
+        if ($useremail === 'silkroadodessy@gmail.com' && $userpassword === 'admin') {
+            // Redirect to homeadmin page
+            header("Location: HomeAdmin.html");
+            exit;
+        } else {
+            $sql = "SELECT * FROM Users WHERE Email='$useremail' AND Password='$userpassword'";
+            $result = $conn->query($sql);
+    
+            if ($result->num_rows > 0) {
+                $_SESSION['logemail'] = $useremail;
+                header("Location: HomeLogged.html");
+                exit;
+            } else {
+                echo "<script>alert('Invalid username or password'); window.location.href='Login.html';</script>";
+            }
+        }
         $conn->close();
      }
 
